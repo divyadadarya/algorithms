b198178af4a6edad540e5686cd5a82d36a1f79b2
Shubhendu Singh <43565190+ItsTimeToGetOut@users.noreply.github.com>
2020-08-01 18:39:41 +0530

    Update and rename Detect_Capital.java to Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
+// 1) Problem for Function named -> detectCapitalUse
+
 // Given a word, you need to judge whether the usage of capitals in it is right or not.
-
 // We define the usage of capitals in a word to be right when one of the following cases holds:
-
 // 1.All letters in this word are capitals, like "USA".
 // 2.All letters in this word are not capitals, like "leetcode".
 // 3.Only the first letter in this word is capital, like "Google".
 // Otherwise, we define that this word doesn't use capitals in a right way.
 
-class Solution {
+class Main{
     public boolean detectCapitalUse(String word) {
         int l = word.length();
         int i;
@@ -21,5 +21,12 @@ class Solution {
                 return false;
         }
         return true;
+    }
+    
+    public static void main(String args[]){
+        Main a = new Main();
+        
+        System.out.println(a.detectCapitalUse("Divya"));
+        
     }
 }
