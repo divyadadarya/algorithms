f419dc92027652a90990ff03f6713c5014680826
divyadadarya <divyadadarya@gmail.com>
2021-06-25 15:48:31 +0530

    binary search tree in java

--- binarySearchTree.java
+++ binarySearchTree.java
@@ -1,5 +1,4 @@
-class Main{
-
+class BinarySearchTree{
 	class Node{
 		int value;
 		Node left;
@@ -20,9 +19,38 @@ class Main{
 	public void printInOrder(Node node){
 		if(node != null){
 			printInOrder(node.left);
-			System.out.println(node.value + " ");
+			System.out.println(node.value);
 			printInOrder(node.right);
 		}
 	}
 
+	public Node insert(Node node, int value){
+
+		if(node == null)
+			return new Node(value);
+
+		if(value < node.value){
+			node.left = insert(node.left, value);
+		}
+
+		else if(value > node.value){
+			node.right = insert(node.right, value);
+		}
+
+		return node;
+	}
+}
+
+class Main{
+
+	public static void main(String[] args){
+		BinarySearchTree tree = new BinarySearchTree();
+
+		tree.root = tree.new Node(13);
+
+		tree.insert(tree.root, 15);
+		tree.insert(tree.root, 7);
+
+		tree.printInOrder(tree.root);
+	}
 }
